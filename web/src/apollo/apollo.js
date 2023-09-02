2d6c43308afe33d802d61a8705bbbbebdd3c1ed1
Jordan Olguin <jordanolguin@iMac.local>
2023-09-02 15:49:33 -0600

    added gh-pages

--- web/src/apollo/apollo.js
+++ web/src/apollo/apollo.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const serverURL =
-  "https://5a8c-2601-8c0-681-ba70-5d70-81da-9d42-bd90.ngrok.io/graphql";
+  "https://dcd7-2601-8c0-681-ba70-5d70-81da-9d42-bd90.ngrok.io/graphql";
 
 const httpLink = new HttpLink({
   uri: serverURL,
