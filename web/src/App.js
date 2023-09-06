ab9399625337176f399bb682bb1ec8c78a178337
Lone1ne <ds@Dereks-MacBook-Air.local>
2023-09-06 13:52:05 -0600

    Updated app.js to use react-router-dom

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ResetPasswordForm from "./components/ResetPassword";
 
 function App() {
   return (
-    <div>
-      <ResetPasswordForm />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route
+            path="/reset-password/:token"
+            element={<ResetPasswordForm />}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
