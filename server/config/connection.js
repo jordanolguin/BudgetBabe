99195bfcda6ee2b3d0171cfbd60c7eb609ad5087
Lone1ne <ds@Dereks-MacBook-Air.local>
2023-09-11 14:04:30 -0600

    Added .env to passwordReset, auth, and connection.js. Updated month stash.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/budget-babe-db"
-);
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = mongoose.connection;
